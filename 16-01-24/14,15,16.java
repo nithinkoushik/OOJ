d3f398a7704bfd5482ee269c2465f0310f2015ab
nithinkoushik <153620277+nithinkoushik@users.noreply.github.com>
2024-01-16 18:43:15 +0530

    Create 14,15,16.java

---
+++ 16-01-24/14,15,16.java
@@ -0,0 +1,14 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        String s1="Hello";
+        String s2="world";
+        System.out.println(s1.concat(s2));
+        String s3="Bmsce College";
+        System.out.println(s3.replace("College","Commege"));
+        String s4="      Hello,friends.      ";
+         System.out.println(s4.trim());
+        
+    }
+}
